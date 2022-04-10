cdc850747f407b5e410a9fc4a4ca55530a8890d8
André Simões <alsimoes@users.noreply.github.com>
2022-04-10 15:59:07 -0300

    Update insert.py
    Adding rollback option to the INSERT.

--- 105/insert.py
+++ 105/insert.py
@@ -1,14 +1,21 @@
 import db
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import sessionmaker
 import random
 
-session = Session(db.engine)
+Session = sessionmaker(db.engine)
 
 for t in range(11,20):
     item_id = random.randint(0,1000)
     price = random.randint(20,50)
 
     tr = db.transactions(t, '2022/04/10', item_id, price)
-    session.add(tr)
+    with Session() as session:
+        session.begin()
+        try:
+            session.add(tr)
+        except:
+            session.rollback()
+            raise
+        else:
+            session.commit()
 
-session.commit()
