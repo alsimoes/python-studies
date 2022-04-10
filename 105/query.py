148346b3b37b84756a6e8d2c62884f1371e9e8d7
André Simões <alsimoes@users.noreply.github.com>
2022-04-10 18:56:18 -0300

    clean up

--- 105/query.py
+++ 105/query.py
@@ -1,5 +1,8 @@
 import db
 from sqlalchemy.orm import Session
+import os
+
+os.system('cls' if os.name == 'nt' else 'clear')
 
 session = Session(db.engine)
 
@@ -11,3 +14,4 @@ print("Transactions with price over 40:")
 
 for s in session.query(db.transactions).filter(db.transactions.price>40):
     print(s)
+
